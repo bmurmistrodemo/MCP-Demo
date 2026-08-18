0d01454adaa1a7cc2ec6aae42dccbb6bb3e68cfb
Brandon Murray <bmurmistro@gmail.com>
2026-08-18 17:40:33 -0600

    reset

--- playwright.config.js
+++ playwright.config.js
@@ -34,6 +34,7 @@ export default defineConfig({
     /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
     trace: 'on-first-retry',
     eyesConfig: {
+      batchName: 'Github Demo',
       apiKey: process.env.APPLITOOLS_API_KEY,
       failTestsOnDiff: false,
     },
